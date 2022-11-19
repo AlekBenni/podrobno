e186fa5e65ba7522da001628716696fb85fb2001
AlekBenni <adeyva@mail.ru>
2022-11-19 21:23:22 +0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='alert bg-dark text-white text-center'>
+      <h1>This is App header</h1>
+      </div>
+      <div className='container'>
+    <Rating/>
+    <Accordion/>       
+      </div>
+
     </div>
   );
+
+}
+
+function Rating(){
+  return (
+    <div>
+      <Star/>
+      <Star/>
+      <Star/>
+    </div>
+  )
+}
+
+function Star(){
+  return (
+      <div>star</div>
+  )
+}
+
+function Accordion(){
+  return(
+    <div>
+      <h3>Menu:</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App;
